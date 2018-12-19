f647543e7058e8c20501b06e29d9d430ee4c18a4
Mehmet <blueshift.zoso@gmail.com>
2018-12-20 00:55:06 +0300

    Update TcpServer.java

--- TcpServer.java
+++ TcpServer.java
@@ -10,30 +10,39 @@ public class TcpServer {
     private static final int POOL_SIZE = 10;
     private ServerSocket ss;
     private ExecutorService executor;
+    private static TcpServer instance = null;
+    private static Object syncObject = new Object();
     
     TaskFactory Factory = new TaskFactory();
     
-    public TcpServer() throws IOException 
-    {
+    public TcpServer() throws IOException {
         ss = new ServerSocket(PORT);
-	executor = Executors.newFixedThreadPool(POOL_SIZE);
+        executor = Executors.newFixedThreadPool(POOL_SIZE);
     }
-    
+
+    private static TcpServer getInstance() throws IOException{
+        synchronized(syncObject){
+            if(instance == null) instance = new TcpServer();
+        }
+        return instance;
+    }
+
     public void listen() throws IOException {
-	System.out.println(this + " up and running");
-	while ( true ) {
+        System.out.println(this + " up and running");
+        while ( true ) {
+        
             executor.execute(Factory.getTaskType("ServerTask", ss.accept()));
-	   // executor.execute(new ServerTask(ss.accept()));
-	}
-    }  
+            //executor.execute(new ServerTask(ss.accept()));
+        }
+    }
 
     public static final void main(String[] args) throws IOException {
-	new TcpServer().listen();
+        TcpServer.getInstance().listen();
     }
 
     @Override
     public String toString() {
-	return "TcpServer [port=" + ss.getLocalPort() + "]";
+        return "TcpServer [port=" + ss.getLocalPort() + "]";
     }
-    
+
 }
